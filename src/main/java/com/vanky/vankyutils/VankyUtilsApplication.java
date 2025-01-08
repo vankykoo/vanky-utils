327c8dd0c9f7e8c685500d6195d88fe35bae4935
vankykoo <3541424964@qq.com>
2025-01-08 21:47:06 +0800

    初始化项目

---
+++ src/main/java/com/vanky/vankyutils/VankyUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.vanky.vankyutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VankyUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VankyUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vanky/vankyutils/VankyUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanky.vankyutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VankyUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
