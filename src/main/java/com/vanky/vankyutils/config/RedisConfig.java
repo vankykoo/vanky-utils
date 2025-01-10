714c5e07fc6fecee4e0afd82f1a8688737bb667f
vankykoo <3541424964@qq.com>
2025-01-10 23:56:00 +0800

    fix:测试分布式锁

---
+++ src/main/java/com/vanky/vankyutils/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.vanky.vankyutils.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // 设置 key 和 value 的序列化方式
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
+}
--- src/test/java/com/vanky/vankyutils/VankyUtilsApplicationTests.java
+++ src/test/java/com/vanky/vankyutils/VankyUtilsApplicationTests.java
@@ -1,13 +1,40 @@
 package com.vanky.vankyutils;
 
+import com.vanky.vankyutils.distributedlock.DistributedLockRedisImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class VankyUtilsApplicationTests {
 
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+
     @Test
-    void contextLoads() {
+    public void testRedisConnect(){
+        DistributedLockRedisImpl distributedLockRedis = new DistributedLockRedisImpl(redisTemplate, 60);
+
+        if (distributedLockRedis.lock("test_lock")) {
+            System.out.println("加锁成功");
+        } else {
+            System.out.println("加锁失败");
+        }
+
+        try {
+            Thread.sleep(3000);
+            if (distributedLockRedis.unlock("test_lock")) {
+                System.out.println("解锁成功");
+            } else {
+                System.out.println("解锁失败");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
 }
